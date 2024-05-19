088c4a2d6e31b2e0dca8debcf5654e9cd74e7a98
Luis Gustavo <luisgustavolg1033@gmail.com>
2024-05-19 12:39:03 -0300

    fix dark mode toggler

--- components/dark-mode-toggle.tsx
+++ components/dark-mode-toggle.tsx
@@ -1,8 +1,9 @@
+"use client"
+
 import Button from "./button";
 import useDarkMode from "@/hooks/useDarkMode";
 import { Moon, Sun } from "lucide-react";
 import { SizesEnum, VariantsEnum } from "./constants";
-import useServerDarkMode from "@/hooks/useServerDarkMode";
 
 interface DarkModeToggleProps {
   defaultTheme?: string;
@@ -11,12 +12,13 @@ interface DarkModeToggleProps {
 export default function DarkModeToggle({
   defaultTheme = "dark",
 }: DarkModeToggleProps) {
-  const theme = useServerDarkMode(defaultTheme);
+  const { theme, toggleTheme } = useDarkMode(defaultTheme);
 
   return (
     <Button
       variant={VariantsEnum.Ghost}
       size={SizesEnum.sm}
+      onClick={toggleTheme}
     >
       {theme === "dark" ? (
         <Moon className="w-4 h-4" />
